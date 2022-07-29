7055e36b08caeb4dad641219c003bee21c16a57a
zhangjianyun <zhangjianyun_kd@163.com>
2022-07-29 11:00:54 +0800

    测试通过反射赋值对象的私有属性

--- src/com/lengedyun/ass/AccessibleTest.java
+++ src/com/lengedyun/ass/AccessibleTest.java
@@ -1,8 +1,10 @@
 package com.lengedyun.ass;
 
+import java.lang.reflect.Field;
+
 /**
- * @title: aaa
- * @description: TODO
+ * @title: AccessibleTest
+ * @description: 反射赋值私有变量
  * @auther: 张健云
  * @date: 2021/4/21 19:42
  */
@@ -29,6 +31,28 @@ public class AccessibleTest {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        Class clazz = null;
+        try {
+            clazz = Class.forName("com.lengedyun.ass.AccessibleTest");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        AccessibleTest at = new AccessibleTest();
+        at.setId(1);
+        at.setName("AT");
+        for (Field f : clazz.getDeclaredFields()) {
+            //f.setAccessible(true);//AccessibleTest类中的成员变量为private,故必须进行此操作
+            try {
+                System.out.println(f.get(at));//获取当前对象中当前Field的value
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
